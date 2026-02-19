3b97a47de0edbcd7cf40787b494b9cf9c0dbdd7a
mithridatem <mathieumithridate@adrar-formation.com>
2026-02-19 11:29:34 +0100

    feat : connexion à la BDD MYSQL

---
+++ src/com/exemple/database/Env.java
@@ -0,0 +1,4 @@
+package com.exemple.database;
+
+public class Env {
+}
---
+++ src/com/exemple/database/Mysql.java
@@ -0,0 +1,21 @@
+package com.exemple.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static com.exemple.database.Env.*;
+
+public class Mysql {
+
+    //Connexion à la BDD
+    private static Connection connexion;
+    static {
+        try {
+            connexion = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
