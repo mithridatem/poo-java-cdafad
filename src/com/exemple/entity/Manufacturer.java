c2c524c6cdc79bd6f7679703ecd529040a8eede4
mithridatem <mathieumithridate@adrar-formation.com>
2026-02-20 09:34:18 +0100

    feat : add repository + crud + entity Manufacturer

---
+++ src/com/exemple/entity/Manufacturer.java
@@ -0,0 +1,41 @@
+package com.exemple.entity;
+
+public class Manufacturer {
+    //Attributs
+    private int id;
+    private String name;
+
+    //Constructeurs
+    public Manufacturer() {}
+    public Manufacturer(String name)
+    {
+        this.name=name;
+    }
+    public Manufacturer(int id, String name)
+    {
+        this.id=id;
+        this.name=name;
+    }
+
+    //Getters et Setters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //méthodes
+    @Override
+    public String toString() {
+        return "Manufacturer{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/com/exemple/repository/ManufacturerRepository.java
@@ -0,0 +1,93 @@
+package com.exemple.repository;
+
+import com.exemple.database.Mysql;
+import com.exemple.entity.Category;
+import com.exemple.entity.Manufacturer;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class ManufacturerRepository {
+    //Attribut
+    private Connection connection;
+
+    //Constructeur
+    public ManufacturerRepository() {
+        this.connection = Mysql.getConnexion();
+    }
+
+    //Méthodes
+    //Méthode pour ajouter une categorie
+    public Manufacturer save(Manufacturer manufacturer) {
+        try {
+            //1 Ecrire la requête
+            String sql = "INSERT INTO manufacturer (`name`) VALUE (?)";
+            //2 préparer la requête
+            PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            //3 Assigner le paramètre
+            stmt.setString(1, manufacturer.getName());
+            //4 Exécuter la requête
+            stmt.executeUpdate();
+            //récupération de l'id (Resultset)
+            ResultSet rs = stmt.getGeneratedKeys();
+            //récupération de l'ID
+            if (rs.next()) {
+                manufacturer = new Manufacturer(
+                    rs.getInt("id"),
+                    manufacturer.getName()
+                );
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return manufacturer;
+    }
+
+    //méthode pour récuperer un categorie par son id
+    public Manufacturer find(int id)
+    {
+        Manufacturer manufacturer = null;
+        try {
+            //1 Ecrire la requête
+            String sql = "SELECT id, `name` FROM manufacturer WHERE id = ?";
+            //2 préparer la requête
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            //3 Assigner le paramètre
+            stmt.setInt(1, id);
+            //4 Exécuter la requête
+            ResultSet rs = stmt.executeQuery();
+            //récupérer le resultat de la requête
+            if (rs.next()) {
+                manufacturer = new Manufacturer(rs.getInt("id"), rs.getString("name"));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return manufacturer;
+    }
+
+    //Méthode pour retourner la liste de toutes les categories (ArrayList)
+    public ArrayList<Manufacturer> findAll()
+    {
+        ArrayList<Manufacturer> manufacturers = new ArrayList<>();
+        try {
+            //1 Ecrire la requête
+            String sql = "SELECT id, `name` FROM category";
+            //2 préparer la requête
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            //4 Exécuter la requête
+            ResultSet rs = stmt.executeQuery();
+            //récupérer le resultat de la requête
+            while (rs.next()) {
+                Manufacturer manufacturer = new Manufacturer(rs.getInt("id"), rs.getString("name"));
+                manufacturers.add(manufacturer);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return manufacturers;
+    }
+}
